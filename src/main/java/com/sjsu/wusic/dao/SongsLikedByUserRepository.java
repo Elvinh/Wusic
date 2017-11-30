c8e4d12d108ddb979e01837614c640baf2e3e9ae
Vishaal Prasad <vishaal56@gmail.com>
2017-11-30 10:05:05 -0800

    adding liked songs to user info page

---
+++ src/main/java/com/sjsu/wusic/dao/SongsLikedByUserRepository.java
@@ -0,0 +1,41 @@
+package com.sjsu.wusic.dao;
+
+import com.sjsu.wusic.model.Song;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class SongsLikedByUserRepository {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public List<Song> findByUserId(int userId) {
+
+        List<Song> songsLikedByUser = new ArrayList<>();
+
+        List<Map<String, Object>> maps =
+                jdbcTemplate.queryForList("SELECT song.song_id, song.name, song.year FROM user_likes_song "
+                		+ "INNER JOIN user INNER JOIN song WHERE user_likes_song.user_id = user.user_id "
+                		+ "AND user_likes_song.song_id = song.song_id AND user.user_id = ?", userId);
+
+        for (Map<String, Object> map : maps) {
+
+            Song song = new Song();
+            song.setId((Integer) map.get("song_id"));
+            song.setName((String) map.get("name"));
+            song.setYear((Integer) map.get("year"));
+
+            songsLikedByUser.add(song);
+
+        }
+
+        return songsLikedByUser;
+
+    }
+}
