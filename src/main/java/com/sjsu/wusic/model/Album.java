cc198528aa32de371f912a121a238823f8e46f1e
elvinh <eltonv93@gmail.com>
2017-11-30 21:10:34 -0800

    removed unneccessary fields

--- src/main/java/com/sjsu/wusic/model/Album.java
+++ src/main/java/com/sjsu/wusic/model/Album.java
@@ -1,21 +1,16 @@
 package com.sjsu.wusic.model;
 
 public class Album {
-	private String id;
 	private String name;
 	private int year;
+	private String artwork;
 	
-	public Album(String id, String name, int year) {
-		this.id = id;
+	public Album(String name, int year) {
 		this.name = name;
 		this.year = year;
 	}
 	public Album() {
 		
-	}
-	
-	public String getId() {
-		return id;
 	}
 	
 	public String getName() {
@@ -26,9 +21,6 @@ public class Album {
 		return year;
 	}
 	
-	public void setId(String string) {
-		this.id = string;
-	}
 	
 	public void setName(String name) {
 		this.name = name;
@@ -37,5 +29,12 @@ public class Album {
 	public void setYear(int year) {
 		this.year = year;
 	}
+	public String getArtwork() {
+		return artwork;
+	}
+	public void setArtwork(String artwork) {
+		this.artwork = artwork;
+	}
+	
 
 }
--- src/main/java/com/sjsu/wusic/model/Artist.java
+++ src/main/java/com/sjsu/wusic/model/Artist.java
@@ -6,15 +6,13 @@ public class Artist {
 	
 	private String id;
 	private String name;
-	private Date birthdate;
-	private String hometown;
+
 	
-	public Artist(String id, String name, Date birthdate, String hometown) {
+	public Artist(String id, String name) {
 		super();
 		this.id = id;
 		this.name = name;
-		this.birthdate = birthdate;
-		this.hometown = hometown;
+
 	}
 
 	public Artist() {
@@ -32,18 +30,6 @@ public class Artist {
 	}
 	public void setName(String name) {
 		this.name = name;
-	}
-	public Date getBirthdate() {
-		return birthdate;
-	}
-	public void setBirthdate(Date birthdate) {
-		this.birthdate = birthdate;
-	}
-	public String getHometown() {
-		return hometown;
-	}
-	public void setHometown(String hometown) {
-		this.hometown = hometown;
 	}
 	
 }
