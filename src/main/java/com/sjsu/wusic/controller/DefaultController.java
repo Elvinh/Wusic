be4141ed11f43fd00eda187bc72dfdeae4fc267d
elvinh <eltonv93@gmail.com>
2017-12-03 00:06:01 -0800

    attempt to fix css for unauthenticated user

---
+++ src/main/java/com/sjsu/wusic/controller/DefaultController.java
@@ -0,0 +1,27 @@
+package com.sjsu.wusic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DefaultController {
+	@GetMapping("/")
+    public String home1() {
+        return "home";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/login")
+    public String admin() {
+        return "login";
+    }
+
+    @GetMapping("/hello")
+    public String user() {
+        return "hello";
+    }
+}
