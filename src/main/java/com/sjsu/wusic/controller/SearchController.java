18e78e1d5a7a2b227d7c80960d96eadba58f2eaa
Vishaal Prasad <vishaal56@gmail.com>
2017-12-04 22:48:45 -0800

    fixing song search function

--- src/main/java/com/sjsu/wusic/controller/SearchController.java
+++ src/main/java/com/sjsu/wusic/controller/SearchController.java
@@ -1,7 +1,9 @@
 package com.sjsu.wusic.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import com.sjsu.wusic.dao.SongRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -22,38 +24,54 @@ public class SearchController {
 
     @Autowired
     private SearchRepository searchDao;
-    
+
+    @Autowired
+    private SongRepository songDao;
+
     @Autowired
     private PlaylistsByUserRepository playlistDao;
 
     @RequestMapping("/search")
     public String artist(Model model, @RequestParam(value = "query") String query, @RequestParam(value = "type") String type) {
-    	
+
         List<Playlist> playlists = playlistDao.playlistsByUser(SecurityContextHolder.getContext().getAuthentication().getName());
         model.addAttribute("playlist_options", playlists);
-        
-    	if(type.equals("song")) {
-    		List<Song> songResults = searchDao.findSongs(query);
-        	model.addAttribute("songs", songResults);
-        	return "displayDiscoverSongs";
-    	}
-    	else if(type.equals("artist")) {
-        	List<Artist> artistResults = searchDao.findArtists(query);
-        	model.addAttribute("artists", artistResults);
-        	return "displayDiscoverArtists";
-    	}
-    	else if(type.equals("genre")) {
+
+        if (type.equals("song")) {
+
+            List<Song> songResults = searchDao.findSongs(query);
+            List<Artist> artists = new ArrayList<>();
+            List<String> albums = new ArrayList<>();
+
+            for(Song song : songResults) {
+                Artist artist = songDao.findArtistOfSong(song.getId());
+                artists.add(artist);
+                String albumName = songDao.findAlbumOfSong(song.getId());
+                albums.add(albumName);
+            }
+
+            model.addAttribute("songs", songResults);
+            model.addAttribute("artists", artists);
+            model.addAttribute("albums", albums);
+
+            return "displayDiscoverSongs";
+
+        } else if (type.equals("artist")) {
+            List<Artist> artistResults = searchDao.findArtists(query);
+            model.addAttribute("artists", artistResults);
+            return "displayDiscoverArtists";
+
+        } else if (type.equals("genre")) {
             List<Genre> genreResults = searchDao.findGenres(query);
-        	model.addAttribute("genres", genreResults);
-        	return "displayGenres";
-    	}
-    	else if(type.equals("album")) {
+            model.addAttribute("genres", genreResults);
+            return "displayGenres";
+
+        } else if (type.equals("album")) {
             List<Album> albumResults = searchDao.findAlbums(query);
-    		model.addAttribute("albums", albumResults);
-    		return "displayDiscoverAlbums";
-    	}
-		return "error";
+            model.addAttribute("albums", albumResults);
+            return "displayDiscoverAlbums";
+        }
 
-
+        return "error";
     }
 }
